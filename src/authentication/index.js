9b501d0a7e1a3738239cbd36f1523ae4bf6ff2bc
Luis Hernandez Ayala <lhernandez@earnest.com>
2020-09-16 11:14:41 -0600

    Defined anyOne auth but not used

--- src/authentication/index.js
+++ src/authentication/index.js
@@ -1,12 +1,14 @@
 const basicToken = require('./basic-token');
 const JWTService = require('./jwt');
+const anyOne = require('./any-one');
 
 module.exports = async server => {
+  server.auth.scheme('anyOneScheme', anyOne.scheme);
+  server.auth.strategy('anyOne', 'anyOneScheme', anyOne.validate);
+
   await server.register(require('hapi-auth-bearer-token'));
   server.auth.strategy('basicToken', 'bearer-access-token', basicToken);
 
   await server.register(require('hapi-jsonwebtoken').plugin);
   server.auth.strategy('jwt', 'hapi-jsonwebtoken', JWTService);
-
-  server.auth.default('jwt');
 };
