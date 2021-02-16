0a11de9e89911b06fb8ddf59fc4eef849762e22f
Luis Hernandez Ayala <lhernandez@earnest.com>
2021-02-15 21:27:00 -0700

    adhoc / loan .env if exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 node ./scripts/preinstall.js
 
+ENV_FILE="$(pwd)/.env"
+if [ -f "$ENV_FILE" ]; then
+  for ENV_VAR in $(cat "$ENV_FILE"); do
+    VAR="$(echo "$ENV_VAR" | cut -d '=' -f 1)"
+    if ! $(env | cut -d '=' -f 1 | grep "^$VARIABLE\$" > /dev/null); then
+      export ${ENV_VAR?}
+    fi
+  done
+fi
+
 set -o pipefail
 exec "$@"
