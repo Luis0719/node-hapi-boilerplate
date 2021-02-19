43b40355634adba772e28f44f2968abd9d25b772
Luis Hernandez Ayala <lhernandez@earnest.com>
2021-02-18 18:41:11 -0700

    add server tests

---
+++ test/server.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+const { testServer } = require('../../../testCommon');
+
+describe('GET /', function () {
+  let server;
+
+  before(async function () {
+    server = await testServer.getTestServer();
+  });
+
+  it('responds with 200', async function () {
+    const res = await server.inject({
+      method: 'get',
+      url: '/',
+    });
+
+    expect(res.statusCode).to.equal(200);
+  });
+});
