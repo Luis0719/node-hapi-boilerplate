c4dd7d46a0f60d85cc7ee11cf0a88689d4bfcfb7
Luis Hernandez Ayala <lhernandez@earnest.com>
2021-02-18 21:02:56 -0700

    Remove dotenv. Loand env vars with entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,15 @@ echo "Running as $APP_ENV"
 
 node ./scripts/preinstall.js
 
+ENV_FILE="$(pwd)/config/${APP_ENV}.env"
+if [ -f "$ENV_FILE" ]; then
+  for ENV_VAR in $(cat "$ENV_FILE"); do
+    VAR="$(echo "$ENV_VAR" | cut -d '=' -f 1)"
+    if ! $(env | cut -d '=' -f 1 | grep "^$VARIABLE\$" > /dev/null); then
+      export ${ENV_VAR?}
+    fi
+  done
+fi
+
 set -o pipefail
 exec "$@"
