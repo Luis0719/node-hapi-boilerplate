447caa85f4edbdcb7af6ad387e5274f7b485f821
Luis Hernandez Ayala <lhernandez@earnest.com>
2021-02-18 17:57:23 -0700

    Add api prefix to all services

--- src/plugins/APIs.js
+++ src/plugins/APIs.js
@@ -3,6 +3,9 @@ const services = ['auth', 'users', 'roles'];
 module.exports = () => {
   const buildServicePlugin = service => ({
     plugin: require(`../services/${service}`),
+    routes: {
+      prefix: `/api/${service}`,
+    },
   });
 
   return services.map(buildServicePlugin);
