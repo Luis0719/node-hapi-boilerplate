1149cd5f73e6ac06ce747a09d182b45b890d61da
Luis Hernandez Ayala <lhernandez@earnest.com>
2021-02-18 22:45:40 -0700

    Reformar request's logging info

--- src/plugins/logging.js
+++ src/plugins/logging.js
@@ -1,16 +1,25 @@
+const _ = require('lodash');
+
 module.exports = {
   name: 'pino-logger',
   version: '1.0.0',
   plugin: require('hapi-pino'),
   options: {
     prettyPrint: process.env.NODE_ENV !== 'production',
-    redact: ['req.headers.authorization'], // Protect sensitive data
+    redact: process.env.NODE_ENV !== 'production' ? ['req.headers.authorization'] : [], // Protect sensitive data
+    logPayload: process.env.NODE_ENV !== 'production',
+    logQueryParams: true,
+    logRequestStart: true,
+    logRequestComplete: true,
     serializers: {
       req: (req) => ({
-        url: `${req.method.toUpperCase()} ${req.url}`,
-        headers: req.headers,
+        url: `${req.method} ${req.url}`,
+        // headers: _.omit(req.headers, ['postman-token']),
+        headers: _.pick(req.headers, ['authorization', 'host']),
       }),
-      res: (res) => res.statusCode,
+      res: (res) => ({
+        code: res.statusCode,
+      }),
     },
   },
 };
