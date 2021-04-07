c271abbce8287c56085b807a03547cc2058ee2a0
Luis Hernandez Ayala <luis.hdez97@hotmail.com>
2021-04-07 01:00:46 -0600

    Feat/use faker on prefabs (#19)
    * refactor(seeder): Separate each seeder into different files
    
    * fix(response): No content response returns 204
    
    * fix(deleteUser/userRuotes): Include noContent response to deleteUser endpoint
    
    * test(user routes): Include deleteUser tests
    
    * WIP
    
    * feat(prefabs): Use faker
    
    * fix(installLocalDeps):
    
    Co-authored-by: Luis Hernandez Ayala <lhernandez@earnest.com>

--- test/services/users/routes.spec.js
+++ test/services/users/routes.spec.js
@@ -1,19 +1,63 @@
 'use strict';
-// const { testServer } = require('../../../testCommon');
+const sinon = require('sinon');
 
-describe('GET /', function () {
-  // let server;
+const methods = require('../../../src/services/users/methods');
+const { db, utils, testServer } = require('../../testCommon');
+
+const { initDatabase } = db;
+
+describe('#users routes', function () {
+  let serverInject;
+  let authorizedHeaders;
 
   before(async function () {
-    // server = await testServer.getTestServer();
+    await initDatabase();
+
+    let admin_role = await db.factories.Role.create({ name: 'admin' });
+    let admin = await db.factories.User.create({ roles: [admin_role.id] });
+
+    serverInject = utils.buildServerInjecter(await testServer.getTestServer());
+    authorizedHeaders = utils.buildAuthorizedJwtHeaders(admin.id);
   });
 
-  it('should not authorize without authentication', async function () {
-    // const res = await server.inject({
-    //   method: 'get',
-    //   url: '/api/users',
-    // });
-    //
-    // expect(res.statusCode).to.equal(401);
+  describe('GET /users')
+
+  describe('DELETE /users', async function () {
+    let route;
+
+    before(function () {
+      route = (userId) => ({
+        url: `/api/users/${userId}`,
+        method: 'DELETE',
+      });
+    })
+
+    it('should return 204 if successful', async function() {
+      const deleteUserStub = sinon.stub(methods, 'deleteUser').callsFake(async () => ({}));
+
+      const res = await serverInject(route(1), authorizedHeaders);
+      expect(res.statusCode).to.be.equal(204);
+
+      deleteUserStub.restore();
+    });
+
+    it('should return 204 even if user does not exists', async function() {
+      const deleteUserStub = sinon.stub(methods, 'deleteUser').callsFake(async () => null);
+
+      const res = await serverInject(route(1), authorizedHeaders);
+      expect(res.statusCode).to.be.equal(204);
+
+      deleteUserStub.restore();
+    });
+
+    it('should return 400 if user id does not exists', async function() {
+      const res = await serverInject(route(), authorizedHeaders);
+      expect(res.statusCode).to.be.equal(400);
+    });
+
+    it('should return 400 if user id is not valid', async function() {
+      const res = await serverInject(route('invalidId'), authorizedHeaders);
+      expect(res.statusCode).to.be.equal(400);
+    });
   });
 });
