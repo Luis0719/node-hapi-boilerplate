ddd628510765c69a8b13c66aa2cb251b0a347dc9
Luis Hernandez Ayala <lhernandez@earnest.com>
2021-04-07 00:21:30 -0600

    fix(installLocalDeps):

--- scripts/register-local-deps.js
+++ scripts/register-local-deps.js
@@ -5,23 +5,15 @@ const packageJson = require('../package');
 const exec = require('child_process').exec;
 
 console.log('*** Creting Symbolic links for custom dependencies into node_modules ***');
-exec('mkdir -p node_modules', function (error, stdout, stderr) {
-  if (error) {
-    // console.error(stderr);
-    // process.exit(1);
-    throw error;
-  }
-
-  var dependencies = Object.keys(packageJson.customDependencies);
-  dependencies.forEach(function (depName) {
-    var filePath = '.' + packageJson.customDependencies[depName];
-    exec(`ln -svnf ${filePath} ./node_modules/${depName}`, function (error, stdout, stderr) {
-      if (error) {
-        // console.error(stderr);
-        // process.exit(1);
-        throw error;
-      }
-      console.log(`Symlinked: ${filePath} -> ./node_modules/${depName}`);
-    });
+var dependencies = Object.keys(packageJson.customDependencies);
+dependencies.forEach(function (depName) {
+  var filePath = '.' + packageJson.customDependencies[depName];
+  exec(`ln -svnf ${filePath} ./node_modules/${depName}`, function (error, stdout, stderr) {
+    if (error) {
+      console.error(stderr);
+      process.exit(1);
+      throw error;
+    }
+    console.log(`Symlinked: ${filePath} -> ./node_modules/${depName}`);
   });
 });
